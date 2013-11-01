0a3248898a1232d9394b040d381a3883156221d2
Jouni Malinen <j@w1.fi>
2013-11-01 12:21:43 +0200

    AOSP: Some P2P connection-in-progress changes
    The p2p_search_pending() function is not used anywhere. That part should
    be reverted.
    
    These came in as part of this AOSP commit:
    
    commit 2fb777c379ad560c309c54b96c2c13291da23f54
    Author: Dmitry Shmidt <dimitrysh@google.com>
    Date:   Wed May 2 12:29:53 2012 -0700
    
        wpa_supplicant: Update to Version 0.8.26 from BRCM
    
        - Add interface command
        - Improve scan handling during P2P
    
        BUG: b/6324527, b/6427094
    
        Change-Id: I4425f44d2a15de0725ba4a1b42cc56e10954b314
        Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- src/p2p/p2p.h
+++ src/p2p/p2p.h
@@ -1780,6 +1780,14 @@ int p2p_in_progress(struct p2p_data *p2p);
 int p2p_search_in_progress(struct p2p_data *p2p);
 
 /**
+ * p2p_search_pending - Check whether there is a deferred P2P SEARCH
+ * @p2p: P2P module context from p2p_init()
+ * Returns: 0 if there is no deferred P2P search or 1 if there is one
+ */
+int p2p_search_pending(struct p2p_data *p2p);
+#endif
+
+/**
  * p2p_other_scan_completed - Notify completion of non-P2P scan
  * @p2p: P2P module context from p2p_init()
  * Returns: 0 if P2P module is idle or 1 if an operation was started
